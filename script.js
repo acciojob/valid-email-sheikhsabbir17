8fff96cc0e94c9ae4649eb160096d2f654ebba88
Sheikh Sabbir <144557992+sheikhsabbir17@users.noreply.github.com>
2026-08-27 11:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+	const pattern = /^[A-Za-z0-9_]+@[A-Za-z0-9_]+(\.[A-Za-z0-9_]{2,3})+$/;
+
+  return pattern.test(str);
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
+
